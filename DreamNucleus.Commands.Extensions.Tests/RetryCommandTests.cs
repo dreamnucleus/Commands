c62d66d6fd449916effa49f1894c761e1b32066d
Dylan McKendry <dylan.mckendry@outlook.com>
2018-10-01 21:44:27 +1000

    Fix test

--- DreamNucleus.Commands.Extensions.Tests/RetryCommandTests.cs
+++ DreamNucleus.Commands.Extensions.Tests/RetryCommandTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Autofac;
+using DreamNucleus.Commands.Extensions.Retry;
 using DreamNucleus.Commands.Extensions.Tests.Common;
 using DreamNucleus.Commands.Results;
 using Xunit;
@@ -12,7 +14,15 @@ namespace DreamNucleus.Commands.Extensions.Tests
         [Fact]
         public async Task ProcessAsync_CommandRetriesOnce_ReturnsResult()
         {
-            var commandProcessor = Helpers.CreateDefaultCommandProcessor();
+            var commandProcessor = Helpers.CreateDefaultCommandProcessor(
+                containerBuilder =>
+                {
+                    containerBuilder.RegisterType<RetryCommandHandler>().As<IAsyncCommandHandler<RetryCommand, Unit>>();
+                },
+                commandsBuilder =>
+                {
+                    commandsBuilder.Use<RetryExceptionPipeline>();
+                });
 
             var result = await commandProcessor.ProcessAsync(new RetryCommand(1));
 
@@ -22,7 +32,15 @@ namespace DreamNucleus.Commands.Extensions.Tests
         [Fact]
         public async Task ProcessAsync_CommandRetriesOnceOnly_ThrowsException()
         {
-            var commandProcessor = Helpers.CreateDefaultCommandProcessor();
+            var commandProcessor = Helpers.CreateDefaultCommandProcessor(
+                containerBuilder =>
+                {
+                    containerBuilder.RegisterType<RetryCommandHandler>().As<IAsyncCommandHandler<RetryCommand, Unit>>();
+                },
+                commandsBuilder =>
+                {
+                    commandsBuilder.Use<RetryExceptionPipeline>();
+                });
 
             await Assert.ThrowsAsync<Exception>(async () => await commandProcessor.ProcessAsync(new RetryCommand(2)));
         }
