f705514ae3080198662c83d818e1a43c9f59b64b
Dylan McKendry <dylan.mckendry@outlook.com>
2018-10-01 21:17:06 +1000

    Cleaning up unique

--- DreamNucleus.Commands.Extensions.Tests/Common/Helpers.cs
+++ DreamNucleus.Commands.Extensions.Tests/Common/Helpers.cs
@@ -1,8 +1,5 @@
 ﻿using Autofac;
 using DreamNucleus.Commands.Autofac;
-using DreamNucleus.Commands.Extensions.Cache;
-using DreamNucleus.Commands.Extensions.Retry;
-using DreamNucleus.Commands.Extensions.Semaphore;
 using DreamNucleus.Commands.Extensions.Unique;
 using DreamNucleus.Commands.Results;
 
@@ -14,25 +11,14 @@ namespace DreamNucleus.Commands.Extensions.Tests.Common
         {
             var containerBuilder = new ContainerBuilder();
 
-            containerBuilder.RegisterType<FakeLockManager>().As<ILockManager>().SingleInstance();
             containerBuilder.RegisterType<FakeUniqueManager>().As<IUniqueManager>().SingleInstance();
-            containerBuilder.RegisterType<FakeCacheManager>().As<ICacheManager>().SingleInstance();
 
-            containerBuilder.RegisterType<SemaphoreCommandHandler>().As<IAsyncCommandHandler<SemaphoreCommand, Unit>>();
-            containerBuilder.RegisterType<SemaphoreHashCommandHandler>().As<IAsyncCommandHandler<SemaphoreHashCommand, int>>();
             containerBuilder.RegisterType<UniqueCommandHandler>().As<IAsyncCommandHandler<UniqueCommand, Unit>>();
-            containerBuilder.RegisterType<CacheCommandHandler>().As<IAsyncCommandHandler<CacheCommand, int>>();
-            containerBuilder.RegisterType<RetryCommandHandler>().As<IAsyncCommandHandler<RetryCommand, Unit>>();
+
 
             var commandsBuilder = new AutofacCommandsBuilder(containerBuilder);
 
-            commandsBuilder.Use<SemaphorePipeline>();
             commandsBuilder.Use<UniquePipeline>();
-
-            commandsBuilder.Use<CacheExecutingPipeline>();
-            commandsBuilder.Use<CacheExecutedPipeline>();
-
-            commandsBuilder.Use<RetryExceptionPipeline>();
 
             containerBuilder.RegisterInstance(commandsBuilder).SingleInstance();
             containerBuilder.RegisterType<AutofacLifetimeScopeService>().As<ILifetimeScopeService>();
