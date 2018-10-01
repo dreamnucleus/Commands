97cc3565578fd72b3e7d14a19945917a503ff930
Dylan McKendry <dylan.mckendry@outlook.com>
2018-10-01 21:24:53 +1000

    Ignore

---
+++ DreamNucleus.Commands.Extensions.Tests/Common/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Autofac;
+using DreamNucleus.Commands.Autofac;
+
+namespace DreamNucleus.Commands.Extensions.Tests.Common
+{
+    public static class Helpers
+    {
+        public static ICommandProcessor CreateDefaultCommandProcessor(
+            Action<ContainerBuilder> containerBuilderAction = null,
+            Action<AutofacCommandsBuilder> autofacCommandsBuilderAction = null)
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilderAction?.Invoke(containerBuilder);
+
+            var commandsBuilder = new AutofacCommandsBuilder(containerBuilder);
+            autofacCommandsBuilderAction?.Invoke(commandsBuilder);
+
+            containerBuilder.RegisterInstance(commandsBuilder).SingleInstance();
+            containerBuilder.RegisterType<AutofacLifetimeScopeService>().As<ILifetimeScopeService>();
+            containerBuilder.RegisterType<CommandProcessor>().As<ICommandProcessor>();
+
+            var container = containerBuilder.Build();
+
+            return container.Resolve<ICommandProcessor>();
+        }
+    }
+}
